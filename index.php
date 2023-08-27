8cacd88f4806b650f1ce16974b89f552289f394e
osarthak <hi99@tutanota.com>
2023-08-27 16:36:07 +0530

    update in ui

--- index.php
+++ index.php
@@ -3,16 +3,18 @@
     <title>LibreY</title>
     </head>
     <body>
-        <form class="search-container" action="search.php" method="get" autocomplete="off">
+        <div class="centered">
+            <form class="search-container" action="search.php" method="get" autocomplete="off">
                 <h1>Libre<span class="Y">Y</span></h1>
-                <input type="text" name="q" autofocus/>
+                <input type="text" placeholder="Search Privately" name="q" autofocus/>
                 <input type="hidden" name="p" value="0"/>
                 <input type="hidden" name="t" value="0"/>
                 <input type="submit" class="hide"/>
-                <div class="search-button-wrapper">
+                <div class="search-button-wrapper hide">
                     <button name="t" value="0" type="submit">Search with LibreY</button>
                     <button name="t" value="3" type="submit">Search torrents with LibreY</button>
                 </div>
         </form>
+        </div>
 
 <?php require "misc/footer.php"; ?>
