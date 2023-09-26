293652cf5c8b245e314e818ce19b6469bfc8f0fa
Dean Sallinen <deza604@gmail.com>
2023-09-26 14:40:18 -0700

    Update header.php
    remove method=POST to allow firefox to add as opensearch provider

--- misc/header.php
+++ misc/header.php
@@ -6,7 +6,7 @@
         <meta name="description" content="A privacy respecting meta search engine."/>
         <meta name="referrer" content="no-referrer"/>
         <link rel="stylesheet" type="text/css" href="static/css/styles.css"/>
-        <link title="LibreY search" type="application/opensearchdescription+xml" href="opensearch.xml?method=POST" rel="search"/>
+        <link title="LibreY search" type="application/opensearchdescription+xml" href="opensearch.xml" rel="search"/>
         <link rel="stylesheet" type="text/css" href="<?php
 $theme = $_REQUEST["theme"] ?? trim(htmlspecialchars($_COOKIE["theme"] ?? "dark"));
                 echo "static/css/" . $theme . ".css";
